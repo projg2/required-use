64dbde364d5e79b2411cd06b7c32bc885b019f70
Alexis Ballier <aballier@gentoo.org>
2017-06-05 09:39:15 +0200

    classify script

---
+++ classify.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+from nsolve import solve
+
+def main(filename):
+    f = open(filename, "r")
+    parse_error={}
+    good={}
+    need_topo_sort={}
+    cyclic={}
+
+    for l in f.readlines():
+        pkg,cons = l.split(' ', 1)
+        solve(cons, pkg=pkg, parse_error=parse_error, good=good,
+                need_topo_sort=need_topo_sort, cyclic=cyclic, reraise=False)
+
+    print("Stats:")
+    print("\tParse error: %i"%(len(parse_error)))
+    print("\tGood: %i"%(len(good)))
+    print("\tNeed topo sort: %i"%(len(need_topo_sort)))
+    print("\tCyclic: %i"%(len(cyclic)))
+
+    for k in cyclic:
+        print("%s: %s"%(k,cyclic[k]))
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
