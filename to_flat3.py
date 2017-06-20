0c68d503e59b94d33b49f776eb1a2bb38ba56aae
Michał Górny <mgorny@gentoo.org>
2017-06-20 18:35:22 +0200

    Include a simpler flattening script

---
+++ to_flat3.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# 3rd version of flattening algo, most trivial by design
+
+import sys
+
+from parser import (parse_string, Flag, Implication, NaryOperator,
+        AnyOfOperator, ExactlyOneOfOperator, AtMostOneOfOperator)
+
+
+def flatten3(ast, conditions=[]):
+    for expr in ast:
+        if isinstance(expr, Flag):
+            yield (conditions, expr)
+        elif isinstance(expr, Implication):
+            for x in flatten3(expr.constraint, conditions+expr.condition):
+                yield x
+        elif isinstance(expr, AnyOfOperator):
+            # || ( a b c ... ) -> [!a !b !c ...]? ( a )
+            yield (conditions+[x.negated() for x in expr.constraint], expr.constraint[0])
+        elif isinstance(expr, AtMostOneOfOperator):
+            # ?? ( a b c ... ) -> a? ( !b !c ... ) [!a b]? ( !c ... ) ...
+            for i in range(0, len(expr.constraint)-1):
+                yield (conditions + [x.negated() for x in expr.constraint[0:i]] + expr.constraint[i:i+1],
+                    [x.negated() for x in expr.constraint[i+1:]])
+        elif isinstance(expr, ExactlyOneOfOperator):
+            for x in flatten3([AnyOfOperator(expr.constraint)], conditions):
+                yield x
+            for x in flatten3([AtMostOneOfOperator(expr.constraint)], conditions):
+                yield x
+        else:
+            raise ValueError('Unknown AST expr: %s' % expr)
+
+
+if __name__ == '__main__':
+    print(list(flatten3(parse_string(sys.argv[1]))))
