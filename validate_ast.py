11b099b73c884bfedfa1a347bcdf867c8cf99792
Michał Górny <mgorny@gentoo.org>
2017-07-09 09:08:55 +0200

    validate_ast: Simplify & flatten logic for n-ary

--- validate_ast.py
+++ validate_ast.py
@@ -6,29 +6,33 @@ from parser import (parse_string, Flag, Implication, NaryOperator,
         AllOfOperator)
 
 
-def validate_ast_passthrough(ast, in_nary=False):
+def validate_ast_passthrough(ast):
     for expr in ast:
         if isinstance(expr, Flag):
             pass
         elif isinstance(expr, Implication):
-            if in_nary:
-                raise ValueError('Implication in n-ary operator')
             assert len(expr.condition) == 1
             assert isinstance(expr.condition[0], Flag)
             validate_ast(expr.constraint)
         elif isinstance(expr, AllOfOperator):
             raise ValueError('All-of operator forbidden')
         elif isinstance(expr, NaryOperator):
-            if in_nary:
-                raise ValueError('Nested n-ary operator!')
-            validate_ast(expr.constraint, in_nary=expr.op)
+            for x in expr.constraint:
+                if isinstance(x, Flag):
+                    pass
+                elif isinstance(x, Implication):
+                    raise ValueError('USE-conditional group in %s operator forbidden' % expr.op)
+                elif isinstance(x, NaryOperator):
+                    raise ValueError('%s group in %s operator forbidden' % (x.op, expr.op))
+                else:
+                    raise NotImplementedError('Unknown AST subexpr: %s' % x)
         else:
             raise NotImplementedError('Unknown AST subexpr: %s' % expr)
         yield expr
 
 
-def validate_ast(ast, in_nary=False):
-    for x in validate_ast_passthrough(ast, in_nary):
+def validate_ast(ast):
+    for x in validate_ast_passthrough(ast):
         pass
 
 
